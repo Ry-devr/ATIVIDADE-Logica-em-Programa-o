75fc7b84424e9247df0880caa6831178fbe3018d
Ry-devr <rtmz.developer@gmail.com>
2026-04-18 11:29:12 -0300

    Questão 1 Feita

---
+++ questao1.py
@@ -0,0 +1,20 @@
+#FOR
+for n in range(8452):
+    print("Ryan")
+
+#WHILE
+contador = 0
+while contador < 8452:
+    contador += 1
+    print("Ryan")
+
+#FUÇÃO RECURCIVA
+def repetir_nome(vezes):
+    if vezes == 0:
+        return
+    else:
+        print("Ryan")
+
+        repetir_nome(vezes-1)
+
+repetir_nome(8452)
