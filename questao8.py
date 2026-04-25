255de78a31e86a3480170a8ae2fab13c2060f187
Ry-devr <rtmz.developer@gmail.com>
2026-04-24 22:42:05 -0300

    Quetão 8 feita e a 7 reformulada

--- questao8.py
+++ questao8.py
@@ -14,7 +14,15 @@ mesPorexteco = [
 ]
 
 dia = int(input("Dia:  "))
+while dia > 31 or dia <= 0:
+    print("data invalida")
+    dia = int(input("Dia:  "))
+
 mes = int(input("Mes:  "))
+while mes > 12 or mes <= 0:
+    print("data invalida")
+    mes = int(input("Mes:  "))
+
 ano = int(input("ano:  "))
 
 print(f"{dia} de {mesPorexteco[mes - 1]} de {ano}")
